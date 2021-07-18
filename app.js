4429a0a45070a1660226a55a36a3827f7711e9a4
Saimun <saimun4u@gmail.com>
2021-07-18 17:09:01 -0500

    List functionality for drag and drop added

--- app.js
+++ app.js
@@ -51,23 +51,39 @@ function createList(){
 }
 
 function dragStart(){
-    console.log('Event: ', 'dragstart')
+    // console.log('Event: ', 'dragstart')
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+    console.log(dragStartIndex)
 }
 
 function dragEnter(){
-    console.log('Event: ', 'dragenter')
+    this.classList.add('over')
+    // console.log('Event: ', 'dragenter')
 }
 
 function dragLeave(){
-    console.log('Event: ', 'dragleave')
+    this.classList.remove('over')
+    // console.log('Event: ', 'dragleave')
 }
 
-function dragOver(){
-    console.log('Event: ', 'dragover')
+function dragOver(e){
+    e.preventDefault();
+    // console.log('Event: ', 'dragover')
 }
 
 function dragDrop(){
-    console.log('Event: ', 'drop')
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+    this.classList.remove('over');
+    // console.log('Event: ', 'drop')
+}
+
+function swapItems(fromIndex, toIndex){
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
 }
 
 function addEventListener(){
